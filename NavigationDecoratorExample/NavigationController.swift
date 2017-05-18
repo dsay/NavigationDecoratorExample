15c03d506722711c4c42b78bcef2796bb7aa8d8a
dsay <dsay@ciklum.com>
2017-05-18 09:58:11 +0300

    commit all files

--- NavigationDecoratorExample/NavigationController.swift
+++ NavigationDecoratorExample/NavigationController.swift
@@ -1,35 +1,68 @@
-//
-//  NavigationController.swift
-//  NavigationDecoratorExample
-//
-//  Created by User on 5/17/17.
-//  Copyright © 2017 Dima Sai. All rights reserved.
-//
-
 import UIKit
+import NavigationDecorator
 
-class NavigationController: UINavigationController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+fileprivate struct Empty: Decorator {
+    func decorate(_ navigationController: UINavigationController) {
+        navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController.navigationBar.shadowImage = UIImage()
+        navigationController.navigationBar.isOpaque = false
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationBar.backgroundColor = .clear
+        navigationController.navigationBar.tintColor = UIColor(red: 147 / 255, green: 166 / 255, blue: 173 / 255, alpha: 1.0)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func decorateBackButton(for viewController: UIViewController, selector: Selector) {
+        let backButton = UIBarButtonItem(image: UIImage(named: "arrow back"),
+                                         style: .plain,
+                                         target: viewController.navigationController,
+                                         action: selector)
+        viewController.navigationItem.leftBarButtonItem = backButton
+        viewController.navigationItem.hidesBackButton = true
     }
-    */
+}
 
+fileprivate struct Blue: Decorator {
+    func decorate(_ navigationController: UINavigationController) {
+        navigationController.navigationBar.isOpaque = false
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationBar.tintColor = .white
+        navigationController.navigationBar.barTintColor = UIColor(red: 29 / 255, green: 182 / 255, blue: 224 / 255, alpha: 1.0)
+        navigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        navigationController.navigationBar.shadowImage = UIImage()
+        navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    }
+    
+    func decorateBackButton(for viewController: UIViewController, selector: Selector) {
+        let backButton = UIBarButtonItem(image: UIImage(named: "back"),
+                                         style: .plain,
+                                         target:viewController.responds(to: selector) ?
+                                            viewController :
+                                            viewController.navigationController,
+                                         action: selector)
+        
+        viewController.navigationItem.leftBarButtonItem = backButton
+        viewController.navigationItem.hidesBackButton = true
+    }
+}
+
+class MainNavigationController: NavigationController {
+    
+    init(nibName nibNameOrNil: String? = nil, bundle nibBundleOrNil: Bundle? = nil){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil, decorator: Blue())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, decorator: Blue())
+    }
+}
+
+class LoginNavigationController: NavigationController {
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil, decorator: Empty())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, decorator: Empty())
+    }
 }
--- NavigationDecoratorExample/ViewController.swift
+++ NavigationDecoratorExample/ViewController.swift
@@ -1,25 +1,6 @@
-//
-//  ViewController.swift
-//  NavigationDecoratorExample
-//
-//  Created by User on 5/17/17.
-//  Copyright © 2017 Dima Sai. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
